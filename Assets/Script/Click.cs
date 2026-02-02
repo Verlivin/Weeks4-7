68f28d8dcec0b3df49d5f26a105c51b47d4bfb8a
Verlivin <dan1829647051@gmail.com>
2026-02-02 09:54:13 -0500

    add sound and duck tp

---
+++ Assets/Script/Click.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class Click : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //move item to mouse when click
+        if(Mouse.current.leftButton.wasPressedThisFrame == true && !EventSystem.current.IsPointerOverGameObject())
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            transform.position = mousePos;
+        }
+    }
+}
