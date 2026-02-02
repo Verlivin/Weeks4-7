7cb92c9169886691daffc082c2789ad167e714e4
Verlivin <dan1829647051@gmail.com>
2026-02-02 09:36:47 -0500

    add moon

---
+++ Assets/Script/T50.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class T50 : MonoBehaviour
+{
+    public float timerValue = 0;
+    public float timerMax = 10;
+    public Slider timerVisuals;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        timerVisuals.maxValue = timerMax;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timerValue += Time.deltaTime;
+
+        if(timerValue > timerMax)
+        {
+            timerValue = 0;
+        }
+        timerVisuals.value = timerValue;
+    }
+}
