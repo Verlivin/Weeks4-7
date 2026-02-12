a1c35349b5ecc00c23dc29d05916ad2b7c6efb64
Verlivin <dan1829647051@gmail.com>
2026-02-12 14:34:51 -0500

    finish score

--- Assets/Script/Charge.cs
+++ Assets/Script/Charge.cs
@@ -3,19 +3,19 @@ using UnityEngine.UI;
 
 public class Charge : MonoBehaviour
 {
-	public SpriteRenderer spriteRenderer;
-	public Color col;
 	public Slider slider;
+	public Slider score;
 	private float chargeDir = 1;
+	private bool raise = false;
 	// Start is called once before the first execution of Update after the MonoBehaviour is created
 	void Start()
-    {
-        
-    }
+	{
 
-    // Update is called once per frame
-    void Update()
-    {
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
 		if (Input.GetKeyDown(KeyCode.Space))
 		{
 			OnSpacePressed();
@@ -25,31 +25,42 @@ public class Charge : MonoBehaviour
 			OnSpaceHolding();
 		}
 
-		
+
 		if (Input.GetKeyUp(KeyCode.Space))
 		{
 			OnSpaceReleased();
+			raise = true;
+		}
+		if(raise == true)
+		{
+			score.value += 1000 * Time.deltaTime;
+			if (score.value >= slider.value)
+			{
+				raise = false;
+			}
+
 		}
 	}
 	void OnSpacePressed()
 	{
 		Debug.Log("uuuu");
 		slider.value = 0;
+		score.value = 0;
 	}
 	void OnSpaceHolding()
 	{
 		Debug.Log("ooooo");
-		if (slider.value <= 0 || slider.value >=1000)
+		if (slider.value <= 0 || slider.value >= 1000)
 		{
 			chargeDir *= -1;
 		}
 		slider.value += chargeDir;
-		
+
 	}
 
 	void OnSpaceReleased()
 	{
 		Debug.Log("aaaa");
-
+		
 	}
 }
